af9b230299a1604f10661561643608f37043f11c
E-Sh4rk <esh4rk@gmail.com>
2018-08-06 19:11:29 +0200

    js graphical interface

--- bin/awale_js.ml
+++ bin/awale_js.ml
@@ -13,3 +13,12 @@ let ia_play depth =
   let move, _ = AI.minmax ~depth:depth (!current) in
   current := (Board.play (!current) move).new_state ;
   move
+
+let _ =
+  Js.export "awale_AI"
+    (object%js
+      method restart = restart ()
+      method human_play x = human_play x
+      method ia_play x = ia_play x
+    end)
+    
